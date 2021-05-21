a9194fca6e8b91894b0bbaa91bd65cafc1e71d9c
Jonathan Engelsma <jengelsma@gmail.com>
2021-05-21 08:41:55 -0400

    Completed part E.

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import ProfileEditScreen from './screens/ProfileEdit';
+import ProfileScreen from './screens/Profile';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
 export default function App() {
+
+  const Stack = createStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="ProfileEdit" component={ProfileEditScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
